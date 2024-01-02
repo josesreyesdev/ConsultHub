c77330a46ec284cf2f9b74e269b5025e58c45e0a
Jose SR <josesanchez09190@gmail.com>
2024-01-01 20:04:03 -0600

    added permission

--- app/src/main/java/com/jsrdev/consulthub/ui/MainActivity.kt
+++ app/src/main/java/com/jsrdev/consulthub/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.jsrdev.consulthub
+package com.jsrdev.consulthub.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.jsrdev.consulthub.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
