7ded2af285fceec478456f3486ca7aab8ca72c92
Jose SR <josesanchez09190@gmail.com>
2024-03-22 16:36:15 -0600

    upgraded ui

--- app/src/main/java/com/jsrdev/consulthub/ui/HomeFragment.kt
+++ app/src/main/java/com/jsrdev/consulthub/ui/HomeFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.jsrdev.consulthub.R
+import com.jsrdev.consulthub.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding by lazy { requireNotNull(_binding) }
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +24,30 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUI()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() { 
+        val navHostFragment = childFragmentManager
+            .findFragmentById(R.id.home_nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNavView.setupWithNavController(navController)
     }
 
 }
